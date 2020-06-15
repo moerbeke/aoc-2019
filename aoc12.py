92f79770c67b893f2ea6aff8948805f607874125
moerbeke <aceballos@gmail.com>
2020-06-15 20:56:47 +0200

    Day 12, part 2

--- aoc12.py
+++ aoc12.py
@@ -12,8 +12,8 @@ day = 12
 # Algorithms
 ########################################################################
 
+import math
 from collections import namedtuple
-from blist import blist
 
 C = namedtuple('C', ['x', 'y', 'z'])
 
@@ -49,6 +49,24 @@ class Moon:
                 self.p.y + self.v.y,
                 self.p.z + self.v.z)
 
+    def move_x(self):
+        self.p = C(
+                self.p.x + self.v.x,
+                self.p.y,
+                self.p.z)
+
+    def move_y(self):
+        self.p = C(
+                self.p.x,
+                self.p.y + self.v.y,
+                self.p.z)
+
+    def move_z(self):
+        self.p = C(
+                self.p.x,
+                self.p.y,
+                self.p.z + self.v.z)
+
     def energy(self):
         return self.potential_energy() * self.kinetic_energy()
 
@@ -79,15 +97,40 @@ def solve_1():
     return energy(moons)
 
 def solve_2():
-    #find_cycle(energy)
-    #find_cycle(get_state_x)
-    #find_cycle(get_state_y)
-    #find_cycle(get_state_z)
-    return
+    moons = reset()
+    x = find_repeated_state_x(moons)
+    y = find_repeated_state_y(moons)
+    z = find_repeated_state_z(moons)
+    xy = abs(x*y) // math.gcd(x, y)
+    return abs(xy*z) // math.gcd(xy, z)
 
 def simulate(moons, steps):
     for i in range(steps):
         step(moons)
+
+def find_repeated_state_x(moons):
+    state_0 = get_state_x(moons)
+    for i in range(1000000):
+        step_x(moons)
+        state = get_state_x(moons)
+        if state == state_0:
+            return i + 1
+
+def find_repeated_state_y(moons):
+    state_0 = get_state_y(moons)
+    for i in range(1000000):
+        step_y(moons)
+        state = get_state_y(moons)
+        if state == state_0:
+            return i + 1
+
+def find_repeated_state_z(moons):
+    state_0 = get_state_z(moons)
+    for i in range(1000000):
+        step_z(moons)
+        state = get_state_z(moons)
+        if state == state_0:
+            return i + 1
 
 def step(moons):
     n_moons = len(moons)
@@ -97,19 +140,52 @@ def step(moons):
     for i in range(0, n_moons):
         apply_velocity(moons[i])
 
+def step_x(moons):
+    n_moons = len(moons)
+    for i in range(0, n_moons-1):
+        for j in range(i+1, n_moons):
+            apply_gravity_x(moons[i], moons[j])
+    for i in range(0, n_moons):
+        apply_velocity_x(moons[i])
+
+def step_y(moons):
+    n_moons = len(moons)
+    for i in range(0, n_moons-1):
+        for j in range(i+1, n_moons):
+            apply_gravity_y(moons[i], moons[j])
+    for i in range(0, n_moons):
+        apply_velocity_y(moons[i])
+
+def step_z(moons):
+    n_moons = len(moons)
+    for i in range(0, n_moons-1):
+        for j in range(i+1, n_moons):
+            apply_gravity_z(moons[i], moons[j])
+    for i in range(0, n_moons):
+        apply_velocity_z(moons[i])
+
 def apply_gravity(m1, m2):
+    apply_gravity_x(m1, m2)
+    apply_gravity_y(m1, m2)
+    apply_gravity_z(m1, m2)
+
+def apply_gravity_x(m1, m2):
     if m1.p.x > m2.p.x:
         m1.decr_vx()
         m2.incr_vx()
     elif m1.p.x < m2.p.x:
         m1.incr_vx()
         m2.decr_vx()
+
+def apply_gravity_y(m1, m2):
     if m1.p.y > m2.p.y:
         m1.decr_vy()
         m2.incr_vy()
     elif m1.p.y < m2.p.y:
         m1.incr_vy()
         m2.decr_vy()
+
+def apply_gravity_z(m1, m2):
     if m1.p.z > m2.p.z:
         m1.decr_vz()
         m2.incr_vz()
@@ -120,31 +196,17 @@ def apply_gravity(m1, m2):
 def apply_velocity(moon):
     moon.move()
 
+def apply_velocity_x(moon):
+    moon.move_x()
+
+def apply_velocity_y(moon):
+    moon.move_y()
+
+def apply_velocity_z(moon):
+    moon.move_z()
+
 def energy(moons):
     return sum([m.energy() for m in moons])
-
-def find_cycle(state_getter):
-    moons = reset()
-    states = blist()
-    state = state_getter(moons)
-    steps = 0
-    while not state in states:
-        states.append(state)
-        step(moons)
-        state = state_getter(moons)
-        steps += 1
-        if steps % 1000 == 0:
-            print(steps)
-    offset = states.index(state)
-    period = len(states) - offset
-    assert(states[offset] == state)
-    print(state)
-    for i in range(period):
-        step(moons)
-        state = state_getter(moons)
-        print(state)
-    print(offset, period)
-    assert(states[offset] == state_getter(moons))
 
 def get_state_x(moons):
     return [m.p.x for m in moons] + [m.v.x for m in moons]
